ce07f7bd0dc183f4eb2691c3c578ea51ad3d0550
花果山大圣 <316783812@qq.com>
2022-10-21 21:00:48 +0800

    chore: update

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,23 @@ import Pages from 'vite-plugin-pages'
 export default defineConfig({
   plugins: [
     Solid(),
-    Pages(),
+    // https://www.npmjs.com/package/vite-plugin-pages
+    Pages({
+      // dirs:[
+        // {dir:"src/pages", baseRoute:""}, // for docs
+      // ],
+      // importMode: 'async', // or sync
+      importMode(filepath, options) {
+        // default resolver
+        // for (const page of options.dirs) {
+        //   if (page.baseRoute === '' && filepath.startsWith(`/${page.dir}/index`))
+        //     return 'sync'
+        // }
+        // return 'async'
+        // Load about page synchronously, all other pages are async.
+        return filepath.includes('about') ? 'async' : 'sync'
+      },
+    }),
     // Inspect()
   ],
   server: {
